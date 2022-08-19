fc3cbfd2f24c7f7467d5867cc45e6cb8a4f4dce0
MONthinkpad <jmontalvo@sanservices.hn>
2022-08-19 14:04:29 -0600

    updated the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-// Copyright (c) Wictor Wilén. All rights reserved. 
+// Copyright (c) Wictor Wilén. All rights reserved.
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
@@ -14,119 +14,134 @@ const argv = require("yargs").argv;
 const debug = argv.debug !== undefined;
 const lint = !(argv["no-linting"] || argv.l === true);
 
-const config = [{
-    entry: {
-        server: [
-            path.join(__dirname, "/src/server/server.ts")
-        ]
-    },
-    mode: debug ? "development" : "production",
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "[name].js",
-        devtoolModuleFilenameTemplate: debug ? "[absolute-resource-path]" : "[]"
-    },
-    externals: [nodeExternals()],
-    devtool: debug ? "source-map" : "source-map",
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        alias: {}
-    },
-    target: "node",
-    node: {
-        __dirname: false,
-        __filename: false
-    },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "ts-loader",
-                options: {
-                    transpileOnly: true
-                }
-            }
-        }]
-    },
-    plugins: [
-        new ForkTsCheckerWebpackPlugin({
-            typescript: {
-                configFile: "./src/server/tsconfig.json"
-            }
-        })
-    ]
-},
-{
-    entry: {
-        client: [
-            path.join(__dirname, "/src/client/client.ts")
-        ]
-    },
-    mode: debug ? "development" : "production",
-    output: {
-        path: path.join(__dirname, "/dist/web/scripts"),
-        filename: "[name].js",
-        libraryTarget: "umd",
-        library: "apmbot",
-        publicPath: "/scripts/"
-    },
-    externals: {},
-    devtool: debug ? "source-map" : "source-map",
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        alias: {}
-    },
-    target: "web",
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "ts-loader",
-                options: {
-                    transpileOnly: true
-                }
-            }
-        }]
-    },
-    plugins: [
-        new webpack.EnvironmentPlugin({ PUBLIC_HOSTNAME: undefined, TAB_APP_ID: null, TAB_APP_URI: null }),
-        new ForkTsCheckerWebpackPlugin({
-            typescript: {
-                configFile: "./src/client/tsconfig.json"
-            }
-        })
-    ],
-    devServer: {
-        hot: false,
-        host: "localhost",
-        port: 9000,
-        allowedHosts: "all",
-        client: {
-            overlay: {
-                warnings: false,
-                errors: true
-            }
+const config = [
+    {
+        entry: {
+            server: [path.join(__dirname, "/src/server/server.ts")]
         },
-        devMiddleware: {
-            writeToDisk: true,
-            stats: {
-                all: false,
-                colors: true,
-                errors: true,
-                warnings: true,
-                timings: true,
-                entrypoints: true
+        mode: debug ? "development" : "production",
+        output: {
+            path: path.join(__dirname, "/dist"),
+            filename: "[name].js",
+            devtoolModuleFilenameTemplate: debug
+                ? "[absolute-resource-path]"
+                : "[]"
+        },
+        externals: [nodeExternals()],
+        devtool: debug ? "source-map" : "source-map",
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+            alias: {}
+        },
+        target: "node",
+        node: {
+            __dirname: false,
+            __filename: false
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "ts-loader",
+                        options: {
+                            transpileOnly: true
+                        }
+                    }
+                }
+            ]
+        },
+        plugins: [
+            new ForkTsCheckerWebpackPlugin({
+                typescript: {
+                    configFile: "./src/server/tsconfig.json"
+                }
+            })
+        ]
+    },
+    {
+        entry: {
+            client: [path.join(__dirname, "/src/client/client.ts")]
+        },
+        mode: debug ? "development" : "production",
+        output: {
+            path: path.join(__dirname, "/dist/web/scripts"),
+            filename: "[name].js",
+            libraryTarget: "umd",
+            library: "apmbot",
+            publicPath: "/scripts/"
+        },
+        externals: {},
+        devtool: debug ? "source-map" : "source-map",
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+            alias: {}
+        },
+        target: "web",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "ts-loader",
+                        options: {
+                            transpileOnly: true
+                        }
+                    }
+                }
+            ]
+        },
+        plugins: [
+            new webpack.EnvironmentPlugin(["NODE_ENV"]),
+            new ForkTsCheckerWebpackPlugin({
+                typescript: {
+                    configFile: "./src/client/tsconfig.json"
+                }
+            })
+        ],
+        devServer: {
+            hot: false,
+            host: "localhost",
+            port: 9000,
+            allowedHosts: "all",
+            client: {
+                overlay: {
+                    warnings: false,
+                    errors: true
+                }
+            },
+            devMiddleware: {
+                writeToDisk: true,
+                stats: {
+                    all: false,
+                    colors: true,
+                    errors: true,
+                    warnings: true,
+                    timings: true,
+                    entrypoints: true
+                }
             }
         }
     }
-}
 ];
 
 if (lint !== false) {
-    config[0].plugins.push(new ESLintPlugin({ extensions: ["ts", "tsx"], failOnError: false, lintDirtyModulesOnly: debug }));
-    config[1].plugins.push(new ESLintPlugin({ extensions: ["ts", "tsx"], failOnError: false, lintDirtyModulesOnly: debug }));
+    config[0].plugins.push(
+        new ESLintPlugin({
+            extensions: ["ts", "tsx"],
+            failOnError: false,
+            lintDirtyModulesOnly: debug
+        })
+    );
+    config[1].plugins.push(
+        new ESLintPlugin({
+            extensions: ["ts", "tsx"],
+            failOnError: false,
+            lintDirtyModulesOnly: debug
+        })
+    );
 }
 
 module.exports = config;
