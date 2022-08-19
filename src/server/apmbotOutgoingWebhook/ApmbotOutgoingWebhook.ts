28be9d66b8a6e392417941f1000ec536f3275544
MONthinkpad <jmontalvo@sanservices.hn>
2022-08-19 13:50:21 -0600

    added identation

--- src/server/apmbotOutgoingWebhook/ApmbotOutgoingWebhook.ts
+++ src/server/apmbotOutgoingWebhook/ApmbotOutgoingWebhook.ts
@@ -2,8 +2,8 @@ import * as builder from "botbuilder";
 import * as express from "express";
 import * as crypto from "crypto";
 import {
-  OutgoingWebhookDeclaration,
-  IOutgoingWebhook,
+    OutgoingWebhookDeclaration,
+    IOutgoingWebhook,
 } from "express-msteams-host";
 
 /**
@@ -11,51 +11,54 @@ import {
  */
 @OutgoingWebhookDeclaration("/api/webhook")
 export class ApmbotOutgoingWebhook implements IOutgoingWebhook {
-  /**
-   * Implement your outgoing webhook logic here
-   * @param req the Request
-   * @param res the Response
-   * @param next
-   */
-  public requestHandler(
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ) {
-    // parse the incoming message
-    const incoming = req.body as builder.Activity;
+    /**
+     * Implement your outgoing webhook logic here
+     * @param req the Request
+     * @param res the Response
+     * @param next
+     */
+    public requestHandler(
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) {
+        // parse the incoming message
+        const incoming = req.body as builder.Activity;
 
-    // create the response, any Teams compatible responses can be used
-    let message: Partial<builder.Activity> = {
-      type: builder.ActivityTypes.Message,
-    };
+        // create the response, any Teams compatible responses can be used
+        let message: Partial<builder.Activity> = {
+            type: builder.ActivityTypes.Message,
+        };
 
-    const securityToken = process.env.SECURITY_TOKEN;
-    if (securityToken && securityToken.length > 0) {
-      // There is a configured security token
-      const auth = req.headers.authorization;
-      const msgBuf = Buffer.from((req as any).rawBody, "utf8");
-      const msgHash =
-        "HMAC " +
-        crypto
-          .createHmac("sha256", Buffer.from(securityToken as string, "base64"))
-          .update(msgBuf)
-          .digest("base64");
+        const securityToken = process.env.SECURITY_TOKEN;
+        if (securityToken && securityToken.length > 0) {
+            // There is a configured security token
+            const auth = req.headers.authorization;
+            const msgBuf = Buffer.from((req as any).rawBody, "utf8");
+            const msgHash =
+                "HMAC " +
+                crypto
+                    .createHmac(
+                        "sha256",
+                        Buffer.from(securityToken as string, "base64")
+                    )
+                    .update(msgBuf)
+                    .digest("base64");
 
-      if (msgHash === auth) {
-        // Message was ok and verified
-        message.text = `Echo: ${incoming.text}`;
-      } else {
-        // Message could not be verified
-        message.text = "Error: message sender cannot be verified";
-      }
-    } else {
-      // There is no configured security token
-      message.text =
-        "Error: outgoing webhook is not configured with a security token";
+            if (msgHash === auth) {
+                // Message was ok and verified
+                message.text = `Echo: ${incoming.text}`;
+            } else {
+                // Message could not be verified
+                message.text = "Error: message sender cannot be verified";
+            }
+        } else {
+            // There is no configured security token
+            message.text =
+                "Error: outgoing webhook is not configured with a security token";
+        }
+
+        // send the message
+        res.send(JSON.stringify(message));
     }
-
-    // send the message
-    res.send(JSON.stringify(message));
-  }
 }
